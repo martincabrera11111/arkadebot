5d95c5ceea6113b8b0d372c1f0e3eb3ef4a3b938
Martin Cabrera <cabrerabluemartin@gmail.com>
2021-07-05 13:18:55 -0300

    Bot

--- index.py
+++ index.py
@@ -1,11 +1,8 @@
-import discord
 import os
-
-from dotenv
+from discord.ext import commands
 from dotenv import load_dotenv
+import urllib.request
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='+')
 
@@ -14,4 +11,4 @@ async def sumar(ctx, num1, num2):
     response = int(num1)+int(num2)
     await ctx.send(response)
 
-bot.run(TOKEN)
+bot.run('ODQzMTk1NTk2NDI0NzQwODc0.YKAVGA.2njS0ops1GrNnuil5rh1Yd-IXac')
